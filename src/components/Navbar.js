36de060f0993dddcdbcf11974ae7d9582fa24f71
Abubakar <haywayaheadshotstrategy.fx@gmail.com>
2022-11-24 10:44:42 -0600

    Add hamburger for mobile version

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,27 +1,52 @@
-import React from 'react';
-import { NavLink } from 'react-router-dom';
+import React, { useState } from 'react';
+import Hamburger from 'hamburger-react';
 import '../styles/navbar.css';
 import logo from './images/planet.png';
 
 function Navbar() {
+  const [isOpen, isClosed] = useState(false);
   return (
-    <header className="nav-div">
+    <nav className="nav-div">
       <section className="logo-div">
         <img className="space-hub-logo" src={logo} alt="Space Hub Logo" />
         <h1 className="logo">Space Travelers&apos; Hub</h1>
       </section>
-      <nav className="nav-links">
-        <NavLink to="/" activeclassname="active-link">
+      <section className="hamburger-div for-phone">
+        <Hamburger
+          color="#0290ff"
+          toggled={isOpen}
+          toggle={isClosed}
+          duration={0.8}
+          label="Show menu"
+        />
+        {isOpen
+          ? (
+            <ul className="nav-popUp-ul">
+              <li className="nav-popUp-li">
+                Rockets
+              </li>
+              <li className="nav-popUp-li">
+                Missions
+              </li>
+              <li className="nav-popUp-li">
+                My Profile
+              </li>
+            </ul>
+          )
+          : null}
+      </section>
+      <ul className="nav-links for-desktop">
+        <li className="nav-desk-li">
           Rockets
-        </NavLink>
-        <NavLink to="/missions" activeclassname="active-link">
+        </li>
+        <li className="nav-desk-li">
           Missions
-        </NavLink>
-        <NavLink to="/profile" activeclassname="active-link">
+        </li>
+        <li className="nav-desk-li">
           My Profile
-        </NavLink>
-      </nav>
-    </header>
+        </li>
+      </ul>
+    </nav>
   );
 }
 
