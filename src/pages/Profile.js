0100e22c7b01c29564a25381a28e0a3e5092066f
Abubakar <haywayaheadshotstrategy.fx@gmail.com>
2022-11-25 09:05:31 -0600

    Refactor to allow filtered missions

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,7 +1,44 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
+import ReservedMissions from '../components/dep/ReservedMissions';
+import '../styles/profile.css';
 
 export default function Profile() {
+  const missions = useSelector((state) => state.missions);
+  const joinedMissions = missions.filter((mission) => mission.reserved !== false);
   return (
-    <div>Profile</div>
+    <>
+      <div className="profile-tables-container">
+        <table className="rocket-mission-container">
+          <thead>
+            <tr>
+              <th>My Missions</th>
+            </tr>
+          </thead>
+          <tbody className="rockets-missions">
+            {
+          joinedMissions.map((joined) => (
+            <ReservedMissions
+              key={joined.id}
+              mission={joined.missionName}
+            />
+          ))
+            }
+          </tbody>
+        </table>
+        <table className="rocket-mission-container">
+          <thead>
+            <tr>
+              <th>My Rockets</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr className="rockets-missions">
+              <td>Under Construction</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    </>
   );
 }
