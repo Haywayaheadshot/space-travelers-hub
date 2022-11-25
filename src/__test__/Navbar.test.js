cb5cb68a22e5158613f7a294266d2e608b793caa
Abubakar <haywayaheadshotstrategy.fx@gmail.com>
2022-11-25 11:24:51 -0600

    Add correct extension

--- src/__test__/Navbar.test.js
+++ src/__test__/Navbar.test.js
@@ -1,6 +1,6 @@
 import { BrowserRouter } from 'react-router-dom';
 import renderer from 'react-test-renderer';
-import Navbar from '../components/NavBar';
+import Navbar from '../components/Navbar';
 
 test('Render Navbar', () => {
   const renderNavbar = renderer.create(<BrowserRouter><Navbar /></BrowserRouter>).toJSON();
